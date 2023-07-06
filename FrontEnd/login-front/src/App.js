4f65105e9da627cab4d4a8b80739c342bc8e40f2
Varun Ganta <gantavarun202@gmail.com>
2023-07-06 12:10:48 +0530

    Add files via upload

---
+++ FrontEnd/login-front/src/App.js
@@ -0,0 +1,14 @@
+import Home from "/home/varun/login-front/src/components/Home.js"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "/home/varun/login-front/src/layout/Navbar.js"
+
+function App() {
+  return (
+    <div className = "App">
+        <Navbar/>
+        <Home/>
+    </div>
+  );
+}
+
+export default App;
